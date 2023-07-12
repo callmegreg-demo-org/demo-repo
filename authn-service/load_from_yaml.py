628c9842346e64b5247e690796a07de3a9b6bab1
Greg Mohler <110078080+CallMeGreg@users.noreply.github.com>
2023-07-12 11:56:43 -0400

    Create load_from_yaml.py

---
+++ authn-service/load_from_yaml.py
@@ -0,0 +1,14 @@
+import yaml
+
+# preload to determine identifier keys
+with self._preloading_env() as env:
+    rendered_yaml = env.get_template(filepath).render()
+    data = yaml.load(rendered_yaml, Loader=yaml.FullLoader)
+    if data:
+        if filename in MULTI_CLASS_FILENAMES:
+            for class_name in data:
+                model_identifiers[class_name] = list(
+                    data[class_name].keys())
+        else:
+            class_name = filename[:filename.rfind('.')]
+            model_identifiers[class_name] = list(data.keys)
